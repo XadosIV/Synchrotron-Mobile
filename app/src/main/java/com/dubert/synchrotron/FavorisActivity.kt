5d6dde0d1577715576dad28e830555f27873c045
XadosIV <jxds.lph@pm.me>
2023-10-13 13:22:40 +0200

    debut favoris

---
+++ app/src/main/java/com/dubert/synchrotron/FavorisActivity.kt
@@ -0,0 +1,11 @@
+package com.dubert.synchrotron
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavorisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favoris)
+    }
+}
